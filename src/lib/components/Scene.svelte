8415f389878a98de152604a96ee7bf08f54f8261
Samual Thorson <samual.j.thor@gmail.com>
2023-07-23 16:23:46 -0500

    init mountain scene

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,141 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+	import { writable } from 'svelte/store';
+	import { spring } from 'svelte/motion';
+	import type { Spring } from 'svelte/motion';
+	import { T, useFrame, useThrelte } from '@threlte/core';
+	import { OrbitControls, Text, interactivity } from '@threlte/extras';
+
+	import Tree from './models/Tree.svelte';
+	import Mountain from './models/Mountain.svelte';
+	import { Color } from 'three';
+
+
+	const getRandomHexColor = () =>
+		`#${Math.floor(Math.random() * 16777215)
+			.toString(16)
+			.padStart(6, '0')}`;
+
+	const invertHexColor = (hexColor: string) => {
+		if (hexColor.startsWith('#')) {
+			hexColor = hexColor.slice(1);
+		}
+		const r = (255 - parseInt(hexColor.slice(0, 2), 16)).toString(16).padStart(2, '0');
+		const g = (255 - parseInt(hexColor.slice(2, 4), 16)).toString(16).padStart(2, '0');
+		const b = (255 - parseInt(hexColor.slice(4, 6), 16)).toString(16).padStart(2, '0');
+
+		return `#${r}${g}${b}`;
+	};
+
+	let showText = false;
+
+	const lightColor = writable('#626476');
+
+	const dirLight = {
+		position: [130, 200, 260] as [number, number, number],
+		color: $lightColor,
+		intensity: 10,
+		shadowMapSize: 2048,
+		shadowCamSideLength: 35
+	};
+
+	let rotationY: number = 0;
+
+	//Sphere controls
+	let scale: Spring<number> = spring(1);
+	const pointerenter = () => {
+		$scale = 1.5;
+		showText = true;
+	};
+	const pointerleave = () => {
+		$scale = 1;
+		showText = false;
+	};
+	const click = () => {
+		// $scale += 1;
+		lightColor.set(getRandomHexColor());
+    scene.background = new Color(invertHexColor($lightColor));
+
+		console.log($lightColor, invertHexColor($lightColor));
+	};
+
+  
+  const { scene } = useThrelte();
+  scene.background = new Color(invertHexColor($lightColor));
+
+	interactivity();
+	useFrame((_, delta) => {
+		rotationY -= 0.5 * delta;
+	});
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
+
+<T.PerspectiveCamera makeDefault position={[60, 110, 240]} fov={35} >
+	<OrbitControls autoRotate autoRotateSpeed={0.5} maxPolarAngle={Math.PI / 2.5} enablePan={false}/>
 </T.PerspectiveCamera>
 
+<T.AmbientLight color={invertHexColor($lightColor)} intensity={2} />
 <T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
-
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
+	position={dirLight.position}
+	color={dirLight.color}
+	intensity={dirLight.intensity}
+	castShadow
+	shadow.mapSize.width={dirLight.shadowMapSize}
+	shadow.mapSize.height={dirLight.shadowMapSize}
+	shadow.camera.top={dirLight.shadowCamSideLength}
+	shadow.camera.right={dirLight.shadowCamSideLength}
+	shadow.camera.bottom={-dirLight.shadowCamSideLength}
+	shadow.camera.left={-dirLight.shadowCamSideLength}
 />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
+<Mountain position={[0, -25, 0]} rotation={[0, Math.PI, 0]} />
+
+<Tree position={[26, -3, 18]} rotation={[0, Math.PI, 0]} />
+<Tree position={[22, -5, 22]} />
+<Tree position={[33, -5.9, 21.5]} rotation={[0, 0, 0]} />
+<Tree position={[24, -17, 36]} rotation={[Math.PI / 10, 0, 0]} />
+
+<T.Group position={[5, 40, 10]}>
+	<Text
+		rotation.y={rotationY}
+		text="Välkommen!"
+		anchorX="center"
+		fontSize={3}
+		castShadow
+		receiveShadow
+	/>
+</T.Group>
+
+<T.Group position={[30, 30, 35]}>
+	{#if showText}
+		<Text castShadow anchorX="center" anchorY={-6} text={$lightColor} fontSize={1} />
+	{/if}
+	<T.Mesh
+		castShadow
+		receiveShadow
+		scale={$scale}
+		on:click={click}
+		on:pointerenter={pointerenter}
+		on:pointerleave={pointerleave}
+	>
+		<T.SphereGeometry args={[2.5, 64, 64]} />
+		<T.MeshStandardMaterial color={$lightColor} />
+	</T.Mesh>
+</T.Group>
+
+<!-- 
+  https://next.threlte.xyz/docs/learn/basics/events
+<T.Mesh
+  on:click={(e) => console.log('click')}
+  on:contextmenu={(e) => console.log('context menu')}
+  on:dblclick={(e) => console.log('double click')}
+  on:wheel={(e) => console.log('wheel')}
+  on:pointerup={(e) => console.log('up')}
+  on:pointerdown={(e) => console.log('down')}
+  on:pointerover={(e) => console.log('over')}
+  on:pointerout={(e) => console.log('out')}
+  on:pointerenter={(e) => console.log('enter')}
+  on:pointerleave={(e) => console.log('leave')}
+  on:pointermove={(e) => console.log('move')}
+  on:pointermissed={() => console.log('missed')}
 />
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+ -->
