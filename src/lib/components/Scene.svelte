8a23095766dcd033d906972ded905963df0c7801
Sam Thorson <Sam@Murphyinc.org>
2024-04-15 19:42:44 -0500

    text background glow

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -58,12 +58,10 @@
 
 <T.Group>
 	<Mountain position={[0, -25, 0]} rotation={[0, Math.PI, 0]} />
-
 	<Tree position={[26, -3, 18]} rotation={[0, Math.PI, 0]} />
 	<Tree position={[22, -5, 22]} />
 	<Tree position={[33, -5.9, 21.5]} rotation={[0, 0, 0]} />
 	<Tree position={[24, -17, 36]} rotation={[Math.PI / 10, 0, 0]} />
-
 	<Gnome castShadow receiveShadow position={[33, -5.3, 20]} rotation={[0, -Math.PI / 1.5, 0]} />
 </T.Group>
 
@@ -73,18 +71,7 @@
 <StarField color={$rgbColorTween} size={0.75} amount={1200} radius={600} speed={7} direction={[-0.75, -0.1, -0.65]}/>
 <StarField color={$rgbColorTween} size={3} amount={1200} radius={3000} speed={2} direction={[0.45, 0.1, -0.35]}/>
 
-
-
-<!-- {#if $showWelcome} -->
-<!-- on:click={(e) => (showWelcome.set(false))} -->
-<!-- position={[-12, 55, -4]} -->
-
-<!-- position={[0, 20, 60]} -->
-
-<!-- TODO: pull text from google doc
-			transition / fade the text changes to match color changes 
-		BIG IDEA: make the text spiral so there are no visible line breaks! just one long strand that wraps around mnt -->
-
+<!-- TODO: make text glow the inverse color -->
 <T.Group rotation={[0, textRotY, 0]}>
 	<Text
 	position={[-4, 25, 70]}
@@ -97,6 +84,9 @@
 	maxWidth={470}
 	curveRadius={-75}
 	textAlign={'justify'}
+	outlineBlur={5}
+	outlineColor={$rgbInvertedColorTween}
+	outlineOpacity={0.5}
 	/>
 </T.Group>
 <!-- {/if} -->
